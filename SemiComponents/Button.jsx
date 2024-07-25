aafc574e6ec684b3685b73e58f3225f45b13eafd
Amit Rai <135054273+Amit0966@users.noreply.github.com>
2024-07-25 21:18:34 +0530

    Add files via upload

---
+++ SemiComponents/Button.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import './Button.css'; // Optional, for custom styles
+
+const Button = ({ nm, onClick }) => {
+  return (
+    <button className="custom-button" onClick={() => onClick(nm)}>
+      {nm}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  nm: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
+
